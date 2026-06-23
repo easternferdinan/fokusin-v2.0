79d13237ab3b20d7885f2959b0846fd1f1a967da
easternferdinan <ferdinandrestu03@gmail.com>
2026-06-23 14:23:11 +0700

    fix(su-adm): handle permission denied on config update
    refs: #15

--- app/Config/FastApi.php
+++ app/Config/FastApi.php
@@ -10,14 +10,17 @@ class FastApi extends BaseConfig
 
     public function __construct()
     {
-        $json = file_get_contents(APPPATH . 'Config/FastApi.json');
+        $json = file_get_contents(WRITEPATH . 'FastApi.json');
         $data = json_decode($json, true);
 
+        // $this->baseUrl = env('API_URL');
+
         if ($data['api_base_url'] == '') {
-            $this->baseUrl = env('API_URL');
+           $this->baseUrl = env('API_URL');
         } else {
-            $this->baseUrl = $data['api_base_url'];
+          $this->baseUrl = $data['api_base_url'];
         }
+
     }
 
     public function getBaseUrl(): string
@@ -28,9 +31,9 @@ class FastApi extends BaseConfig
     public function setBaseUrl(string $newBaseUrl): void
     {
         $this->baseUrl = $newBaseUrl;
-        $json = file_get_contents(APPPATH . 'Config/FastApi.json');
+        $json = file_get_contents(WRITEPATH . 'FastApi.json');
         $data = json_decode($json, true);
         $data['api_base_url'] = $newBaseUrl;
-        file_put_contents(APPPATH . 'Config/FastApi.json', json_encode($data));
+        file_put_contents(WRITEPATH . 'FastApi.json', json_encode($data));
     }
 }
